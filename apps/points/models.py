8a7d1e3d6ffa9663c1aa1d92f9462e3150156d8e
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-14 17:05:02 -0600

    migrations

--- apps/points/models.py
+++ apps/points/models.py
@@ -6,3 +6,16 @@ class PointsManager(models.Model):
     account = models.OneToOneField(
         MainUser, on_delete=models.CASCADE, related_name='points')
     total = models.PositiveIntegerField(default=0)
+
+    def __str__(self) -> str:
+        return f'Puntos - {self.account.name}'
+
+    def add_points(self, points: int):
+        self.total + points
+        self.save()
+
+    def redeem_points(self, points: int):
+        self.total - points
+        if self.total < 0:
+            return False
+        self.save()
