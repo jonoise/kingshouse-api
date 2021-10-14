b6448b8d56d28d101ac57d4065aa11f7b0add90b
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-14 17:02:32 -0600

    add products app

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,4 +1,9 @@
+import os
 from django.db import models
+
+
+def image_path(self, filename):
+    return os.path.join('products', filename)
 
 
 class Category(models.Model):
@@ -14,20 +19,25 @@ class Category(models.Model):
     def get_absolute_url(self):
         return f'/{self.slug}/'
 
+    def products(self):
+        return self.products.all()
+
 
 class Product(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=155)
+    description = models.TextField()
+    image = models.ImageField(upload_to=image_path)
     slug = models.SlugField()
     price = models.PositiveIntegerField()
-    points = models.PositiveIntegerField()
+    points = models.PositiveIntegerField(blank=True, null=True)
 
     def __str__(self) -> str:
         return self.name
 
     def get_absolute_url(self):
-        return f'/{self.category.get_absolute_url}/{self.slug}'
+        return f'/{self.category.slug}/{self.slug}'
 
 
 class Ingredient(models.Model):
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import views, permissions, status
+from rest_framework.response import Response
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
-# Create your views here.
+
+class CategoryView(views.APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        category = Category.objects.all()
+        s = CategorySerializer(category, many=True)
+        return Response(data=s.data, status=status.HTTP_200_OK)
+
+
+class ProductView(views.APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        p = Product.objects.all()
+        s = ProductSerializer(p, many=True)
+        return Response(data=s.data, status=status.HTTP_200_OK)
