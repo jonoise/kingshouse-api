7b0c27e3536c2cebcb495a47c7c4f3577b72c29b
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-19 00:28:05 -0600

    edit order models

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,12 +1,39 @@
 from django.db import models
 from apps.accounts.models import MainUser
 from apps.products.models import Product
-# Create your models here.
+from django.contrib.postgres.fields import ArrayField
 
 
 class Order(models.Model):
+    code = models.CharField(max_length=255)
     user = models.ForeignKey(
-        MainUser, on_delete=models.CASCADE, related_name='orders')
+        MainUser, on_delete=models.CASCADE, related_name='orders', blank=True, null=True)
+    valid = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f'Order {self.code}'
+
+
+class OrderProduct(models.Model):
+    code = models.CharField(max_length=255)
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='products')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='order_products')
+    price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=1)
+    ingredients = ArrayField(models.CharField(
+        max_length=255, blank=True, null=True))
+    extras = ArrayField(models.CharField(
+        max_length=255, blank=True, null=True))
+
+    def __str__(self) -> str:
+        return f'Producto ordenado: {self.product.name}'
+
+
+class BillingAddress(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='address')
     full_name = models.CharField(max_length=155)
     state = models.CharField(max_length=155)
     city = models.CharField(max_length=155)
@@ -14,19 +41,4 @@ class Order(models.Model):
     phone = models.PositiveIntegerField()
 
     def __str__(self) -> str:
-        return self.name
-
-    def get_absolute_url(self):
-        return f'/{self.category.get_absolute_url}/{self.slug}'
-
-
-class OrderProduct(models.Model):
-    order = models.ForeignKey(
-        Order, on_delete=models.CASCADE, related_name='products')
-    product = models.ForeignKey(
-        Product, on_delete=models.CASCADE, related_name='order_products')
-    price = models.PositiveIntegerField()
-    quantity = models.PositiveIntegerField(default=1)
-
-    def __str__(self) -> str:
-        return f'Producto ordenado: {self.product.name}'
+        return f'Dirección orden {self.order.code}'
