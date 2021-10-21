b1c48bb40d6e76194fcc35037a31080c1a980278
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-21 01:37:51 -0600

    prod

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -2,7 +2,7 @@ import os
 from .base import *
 import django_on_heroku
 
-DEBUG = True
+DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
 CORS_ALLOWED_ORIGINS = ['https://www.kingshousecr.com']
 
