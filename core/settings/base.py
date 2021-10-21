6f68b4debe287dad5ebc6135f7a099d2c5e424a0
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-21 02:24:37 -0600

    add static root

--- core/settings/base.py
+++ core/settings/base.py
@@ -108,6 +108,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
