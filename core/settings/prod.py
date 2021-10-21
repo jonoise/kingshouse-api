27378df2e9e185f851b44a4a99dded50e39d0939
Amilkar Munoz <yellowballpython@gmail.com>
2021-10-20 19:12:21 -0600

    add prefix to allowed origins

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -4,6 +4,6 @@ import django_on_heroku
 
 DEBUG = True
 SECRET_KEY = os.environ.get('SECRET_KEY')
-CORS_ALLOWED_ORIGINS = ['kingshousecr.com']
+CORS_ALLOWED_ORIGINS = ['https://www.kingshousecr.com']
 
 django_on_heroku.settings(locals())
